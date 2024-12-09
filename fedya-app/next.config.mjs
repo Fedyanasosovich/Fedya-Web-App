c885805f54b8ca82dc5bc02b083b86b510562b3b
Hashir Akbar <hashirbusinessinfo@gmail.com>
2024-12-10 02:34:22 +0500

    added domain update in next config

--- fedya-app/next.config.mjs
+++ fedya-app/next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["minio-rwgosso4gwg0sw8cws4soccs.77.37.87.117.sslip.io"],
+    domains: [
+      "minio-rwgosso4gwg0sw8cws4soccs.77.37.87.117.sslip.io",
+      "buypfizergenotropinhgh.com",
+    ],
   },
 };
 
